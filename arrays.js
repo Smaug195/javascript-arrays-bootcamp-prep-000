b5d65c17db8e4d7e56174d80cbf1e8c9f541df7b
Eli Draluk <eli.draluk@gmail.com>
2018-02-22 20:23:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,6 @@ function removeElementFromBeginningOfArray (arr){
 function destructivelyRemoveElementFromEndOfArray (arr){
   return arr.pop()
 }
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0,arr.length-1)
+}
