cc9469d1c602f44bf3720fd52ccc8896afdb3042
Eli Draluk <eli.draluk@gmail.com>
2018-02-22 20:19:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,12 +7,12 @@ var chocolateBars=[
 function addElementToBeginningOfArray(arr,add){
   return [add,...arr]
 }
-function descructivelyAddElementToBeginningOfArray(arr,add){
+function destructivelyAddElementToBeginningOfArray(arr,add){
   return arr.unshift(add)
 }
 function addElementToEndOfArray(arr,add){
   return [...arr,add]
 }
-function descructivelyAddElementToBeginningOfArray(arr,add){
+function destructivelyAddElementToBeginningOfArray(arr,add){
   return arr.push(add)
 }
